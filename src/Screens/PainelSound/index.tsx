62170b31cef521cfcd6192802111c76b8ca9c9ef
Jhonata Carvalho <jhonata.carvalho@tascomtecnologia.com.br>
2023-12-07 19:22:40 -0300

    initial sound feat

--- src/Screens/PainelSound/index.tsx
+++ src/Screens/PainelSound/index.tsx
@@ -1,17 +1,63 @@
-import { View, Text, Image, Dimensions, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { useNavigation, useRoute } from '@react-navigation/native'
+import React, { useEffect, useRef } from 'react'
+import { View, Image, Dimensions } from 'react-native'
+import { Audio } from 'expo-av'
+import { useRoute } from '@react-navigation/native'
 import Controller from '../../components/PlayerController'
 import Header from '../../components/Header'
-import { AntDesign } from '@expo/vector-icons'
+import { AntDesign, Ionicons } from '@expo/vector-icons'
 import { style } from './style'
 import Icon from '../../components/Icon'
+
 const { width, height } = Dimensions.get('window')
 
-export default function PainelSound() {
+export default function PainelSound({ navigation }) {
   const params: any = useRoute().params
   const [state, setState] = React.useState(false)
+  const soundRef = useRef<Audio.Sound | null>(null)
 
+  async function playSound() {
+    if (!state) {
+      setState(true)
+
+      const { sound } = await Audio.Sound.createAsync(
+        require('../../assets/sounds/rain.mp3'),
+        { isLooping: true },
+      )
+
+      soundRef.current = sound
+      await sound.playAsync()
+    } else {
+      setState(false)
+      await soundRef.current?.stopAsync()
+    }
+  }
+
+  const handleBlur = () => {
+    soundRef.current?.stopAsync()
+  }
+
+  const handleFocus = () => {
+    playSound()
+  }
+
+  useEffect(() => {
+    const unsubscribeBlur = navigation.addListener('blur', handleBlur)
+    const unsubscribeFocus = navigation.addListener('focus', handleFocus)
+
+    return () => {
+      unsubscribeBlur()
+      unsubscribeFocus()
+    }
+  }, [navigation])
+
+  useEffect(() => {
+    return () => {
+      if (soundRef.current) {
+        console.log('Unloading Sound')
+        soundRef.current.unloadAsync()
+      }
+    }
+  }, [])
   return (
     <View style={{ flex: 1 }}>
       <Image
@@ -33,11 +79,11 @@ export default function PainelSound() {
           size={28}
           color="#FFF"
         />
-        <Controller onPress={() => setState(!state)} changeState={state} />
+        <Controller onPress={() => playSound()} changeState={state} />
         <Icon
           onPress={() => console.log()}
-          icon={AntDesign}
-          name="bars"
+          icon={Ionicons}
+          name="volume-low"
           size={28}
           color="#FFF"
         />
